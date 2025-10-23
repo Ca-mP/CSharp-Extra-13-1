c3ad2570bb21a51261a56f80d28eb75ac25003b4
Ca-mP <ckpatterson1s@capetigers.com>
2025-10-23 13:21:37 -0500

    Add files via upload

---
+++ InventoryMaintenance/InventoryMaintenance/InventoryItemList.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace InventoryMaintenance
+{
+    public class InventoryItemList
+    {
+        private List<InventoryItem> items;
+
+        public event ChangeHandler changed;
+
+        public delegate void ChangeHandler(InventoryItemList list);
+
+        public InventoryItemList()
+        {
+            items = new List<InventoryItem>();
+        }
+
+        public int Count => items.Count;
+
+        public InventoryItem this[int i] 
+        {
+            get
+            {
+                if (i > items.Capacity)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    return items[i];
+                }
+            }
+
+            set
+            {
+                if (i > items.Capacity)
+                {
+                    items[i] = value;
+                }
+            }
+
+        }
+
+        public static InventoryItemList operator +(InventoryItemList list, InventoryItem item)
+        {
+            list.Add(item);
+            
+            return list;
+        }
+
+        public static InventoryItemList operator -(InventoryItemList list, InventoryItem item)
+        {
+            list.Remove(item);
+            return list;
+        }
+
+        public void Add(InventoryItem item) => items.Add(item);
+
+        public void Add(int itemNo, string description, decimal price)
+        {
+            InventoryItem i = new(itemNo, description, price);
+            items.Add(i);
+        }
+
+        public void Remove(InventoryItem item) => items.Remove(item);
+
+        public void Fill() => items = InventoryDB.GetItems();
+
+        public void Save() => InventoryDB.SaveItems(items);
+    }
+}
